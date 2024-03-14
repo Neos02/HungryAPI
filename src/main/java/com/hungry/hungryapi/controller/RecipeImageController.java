461f086b6bd6ca65c1785765e06225d76f4465d3
Nicholas Kennedy <nmk10062@gmail.com>
2024-03-14 14:40:54 -0400

    Update image upload to use PUT instead of POST

--- src/main/java/com/hungry/hungryapi/controller/RecipeImageController.java
+++ src/main/java/com/hungry/hungryapi/controller/RecipeImageController.java
@@ -12,7 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +51,7 @@ public class RecipeImageController {
         }
     }
 
-    @PostMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<RecipeImage> uploadImage(@PathVariable("id") long recipeId, @RequestParam("image") MultipartFile file) {
         String outputType = "png";
 
